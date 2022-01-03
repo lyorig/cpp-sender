74cd5e96299c06a24132a8d635a63b8d90da8d11
lyorig <petrik.sacha@gmail.com>
2022-01-03 15:39:12 +0100

    Odstranění system příkazů, možnost změnit locale a nějaké menší úpravy.

--- spam.cpp
+++ spam.cpp
@@ -1,4 +1,7 @@
 #define NOMINMAX
+#define WHITE FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE
+#define YELLOW FOREGROUND_RED | FOREGROUND_GREEN
+#define BGREEN FOREGROUND_GREEN | FOREGROUND_INTENSITY
 
 #include <iostream>
 #include <string>
@@ -13,10 +16,11 @@ int main() {
     
     string ans;
     UINT codepage;
-    string locale;
+    string sLocale;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
     setlocale(LC_ALL, "cs_CZ");
-    system("Color 0E"); // Žlutá
+    SetConsoleTextAttribute(hConsole, YELLOW);
     
     cout << "Chcete editovat nastavení? (ano/ne): ";
     cin >> ans;
@@ -25,23 +29,26 @@ int main() {
 
         cout << "\nProdleva mezi repeticemi (ms): ";
         cin >> delay;
-        cout << "Prodleva nastavena na " << delay << "ms.\n\n";
+        cout << "Prodleva nastavena na základní (" << delay << ")ms.\n\n";
 
         cout << "Codepage: ";
         cin >> codepage;
         
         cout << "Locale: ";
-        cin >> locale;
+        cin >> sLocale; 
 
     }
     else { // Cokoliv jiného než "ano" se bere jako ne
+        
         delay = 30;
         cout << "\nProdleva nastavena na základní (" << delay << "ms).\n";
+
         codepage = 1250;
         locale = "cs_CZ";
+
     }
-    setlocale(LC_ALL, locale);
-    cout << "Locale nastaven na základní (" << locale << ").\n";
+    setlocale(LC_ALL, sLocale);
+    cout << "Locale nastaven na základní (" << sLocale << ").\n";
     SetConsoleCP(codepage);
     cout << "Codepage nastaven na základní (" << codepage << ").\n\n";
 
@@ -49,11 +56,10 @@ int main() {
     cout << "Použitím programu souhlasíte se samostatností Míkovic.\n";
     cout << "Co chcete spamovat? -> ";
     
-    
     cin.ignore(numeric_limits<streamsize>::max(), '\n'); // jinak nebude kvůli cin-u fungovat getline
     getline(wcin, wts);
 
-    system("Color 0A"); // Zelená
+    SetConsoleTextAttribute(hConsole, BGREEN);
 
     cout << "\nHotovo. Držte F12 pro spam.\n"; 
     
@@ -64,5 +70,6 @@ int main() {
         }
         
     }
+    SetConsoleTextAttribute(hConsole, WHITE);
     return 0;
 }
